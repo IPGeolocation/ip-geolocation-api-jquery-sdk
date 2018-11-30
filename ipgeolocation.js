db90ac6fd8311f3c3d443cb8c6d67d1ec594f534
Usman <usman@jfreaks.com>
2018-11-30 17:19:13 +0500

    Multilang support added. Removed all the unnecessary methods to get geolocation and timezone.

--- ipgeolocation.js
+++ ipgeolocation.js
@@ -1,115 +1,95 @@
-        function geolocation(callback) {
-	request("ipgeo", "", "", "", "", "", "", callback)
-	} 
+var ipAddressParameter = "";
+var excludesParameter = "";
+var fieldsParameter = "";
+var langParameter = "";
+var tzParameter = "";
+var latitudeParameter = "";
+var longitudeParameter = "";
 
-        function geolocationByAPIkey(apikey="",callback) {
-        request("ipgeo", apikey, "", "", "", "", "", callback)
-	}        
-        
-        function geolocationByIP(ip="",callback) {
-	request("ipgeo", "", "", ip, "", "", "", callback)
-	}
+function setIPAddressParameter (ip = "") {
+    ipAddressParameter = ip;
+}
 
-        function geolocationByAPIkeyAndIP(apikey="", ip="", callback) {
-        request("ipgeo", apikey, "", ip, "", "", "", callback)
-	}
+function setExcludesParameter (excludes = "") {
+    excludesParameter = excludes;
+}
 
-        function geolocationByFieldsAndIP(fields="", ip="", callback) {
-        request("ipgeo", "", fields, ip, "", "", "", callback)
-	}	
+function setFieldsParameter (fields = "") {
+    fieldsParameter = fields;
+}
 
-        function geolocationByAPIkeyFieldsAndIp(apikey="", fields="", ip="", callback) {
-	request("ipgeo", apikey, fields, ip, "", "", "", callback)
-	}
+function setLanguageParameter (lang = "") {
+    langParameter = lang;
+}
 
-	function geolocationByFields(fields="", callback) {
-	request("ipgeo", "", fields, "", "", "", "", callback)
-	}
+function setTimezoneParameter (tz = "") {
+    tzParameter = tz;
+}
 
-	function geolocationByAPIkeyAndFields(apikey="", fields="", callback) {
-	request("ipgeo", apikey, fields, "", "", "", "", callback)
-	}
+function setCoordinatesParameter (latitude = "", longitude = "") {
+    latitudeParameter = latitude;
+    longitudeParameter = longitude;
+}
 
-	function timezoneByIP(ip="", callback) {
-	request("timezone", "", "", ip, "", "", "", callback)
-	}
+function getGeolocation (apiKey = "", callback) {
+    request("ipgeo", apiKey, callback)
+}
 
-	function timezoneByAPIKeyAndIP(apikey="", ip="", callback) {
-	request("timezone", apikey, "", ip, "", "", "", callback)
-	}
+function getTimezone (apikey = "", callback) {
+    request("timezone", apikey, callback)
+}
 
-        function timezoneByTz(tz="", callback) {
-	request("timezone", "", "", "", tz, "", "", callback)
-	}
-
-	function timezoneByAPIKeyAndTz(apikey="", tz="", callback) {
-	request("timezone", apikey, "", "", tz, "", "", callback)
-	}
-        
-        function timezone(callback) {
-	request("timezone", "", "", "", "", "", "", callback)
-	}
-
-        function timezoneByAPIKey(apikey="", callback) {
-	request("timezone", apikey, "", "", "", "", "", callback)
-	}
-
-        function timezoneByLatitudeAndLongitude(latitude="", longitude="", callback){
-	request("timezone", "", "", "", "", latitude, logitude, callback)
-        }
-
-        function timezoneByAPIKeyLatitudeAndLongitude(apikey="", latitude="", longitude="", callback){
-	request("timezone", apikey, "", "", "", latitude, logitude, callback)
-        }
-
-
-
-
-	function request(subUrl=null, apiKey="",fields="",ip="", tz="", latitude="", longitude="", callback){
-	var URL = "";
-	if(apiKey){
-	URL = subUrl;
-	URL = URL + ("?apiKey=" + apiKey);
-	if(fields){
-	URL = URL + "&fields=";
-	URL = URL + fields;
-	}
-	if(ip){
-	URL = URL + "&ip=";
-	URL = URL + ip;
-	}
-        if(tz){
-	URL = URL + "&tz=";
-	URL = URL + tz;
-	}
-        if(latitude){
-        URL = URL + "&lat=";
-	URL = URL + latitude;
-        }
-        if(longitude){
-        URL = URL + "&long=";
-	URL = URL + longitude;
-        }
-
-	}
-
-	$.ajax(
-        {
-           async: true,
-	  method: "GET",
-	  url: "https://api.ipgeolocation.io/"+URL+"",
-	  contentType: "application/json; charset=utf-8",
-	  dataType: "json",
-	  success: function (data, status, jqXHR) {
-          if(callback) {
-           callback(data);
-           }
-	  },
-	  error: function (data, jqXHR, status) {
-          if(callback) {
-           callback(data.responseText);
-           }
-	  }
-	 } );
+function request (subUrl = null, apiKey = "", callback) {
+    var url = "";
     
-	}
+    if (apiKey) {
+        url = subUrl + "?apiKey=" + apiKey;
+    } else {
+        if (callback) {
+            callback({"status": 401, "message": "You cannot use IP Geolocation API without an API Key"});
+        }
+        return;
+    }
+
+    if (excludesParameter) {
+        url = url + "&excludes=" + excludesParameter;
+    }
+        
+    if (fieldsParameter) {
+        url = url + "&fields=" + fieldsParameter;
+    }
+
+    if (ipAddressParameter) {
+        url = url + "&ip=" + ipAddressParameter;
+    }
+
+    if (langParameter) {
+        url = url + "&lang=" + langParameter;
+    }
+    
+    if (tzParameter) {
+        url = url + "&tz=" + tzParameter;
+    }
+    
+    if (latitudeParameter && longitudeParameter) {
+        url = url + "&lat=" + latitudeParameter + "&long=" + longitudeParameter;
+    }
+
+    $.ajax ({
+        async: true,
+        method: "GET",
+        url: "https://api.ipgeolocation.io/" + url + "",
+        contentType: "application/json",
+        dataType: "json",
+        success: function (data, status, jqXHR) {
+            if (callback) {
+                callback(data);
+            }
+        },
+        error: function (data, jqXHR, status) {
+            if (callback) {
+                callback(data.responseText);
+            }
+        }
+    });
+}
