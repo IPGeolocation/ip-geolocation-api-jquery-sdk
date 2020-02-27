b167b40e8775ba6125655c13e8d022ebb3bb66e1
Usman Liaqat <usman@jfreaks.com>
2020-02-27 15:44:53 +0500

    Built _ipgeolocation namespace and put all the public methods under it make the SDK work under _ipgeolocation namespace avoiding contaminating the global JavaScript workspaces

--- ipgeolocation.js
+++ ipgeolocation.js
@@ -1,132 +1,127 @@
-var ess = false;
-var asyncParameter = true;
-var ipAddressParameter = "";
-var excludesParameter = "";
-var fieldsParameter = "";
-var langParameter = "";
-var tzParameter = "";
-var latitudeParameter = "";
-var longitudeParameter = "";
+var _ipgeolocation = function() {
+    var useSessionStorage = false;
+    var asyncCall = true;
+    var ipAddress = "";
+    var excludes = "";
+    var fields = "";
+    var lang = "en";
+    var tz = "";
+    var latitude = "";
+    var longitude = "";
 
-function enableSessionStorage(enable = false) {
-    ess = enable;
-}
-
-function setAsync(async = true) {
-    asyncParameter = async;
-}
-
-function setIPAddressParameter(ip = "") {
-    ipAddressParameter = ip;
-}
-
-function setExcludesParameter(excludes = "") {
-    excludesParameter = excludes;
-}
-
-function setFieldsParameter(fields = "") {
-    fieldsParameter = fields;
-}
-
-function setLanguageParameter(lang = "") {
-    langParameter = lang;
-}
-
-function setTimezoneParameter(tz = "") {
-    tzParameter = tz;
-}
-
-function setCoordinatesParameter(latitude = "", longitude = "") {
-    latitudeParameter = latitude;
-    longitudeParameter = longitude;
-}
-
-function getGeolocation(callback, apiKey = "") {
-    request("ipgeo", callback, apiKey);
-}
-
-function getTimezone(callback, apikey = "") {
-    request("timezone", callback, apikey);
-}
-
-function addUrlParameter(parameters, parameterName, parameterValue) {
-    if (parameters) {
-        parameters = parameters.concat("&", parameterName, "=", parameterValue);
-    } else {
-        parameters = "?".concat(parameterName, "=", parameterValue);
-    }
-    
-    return parameters;
-}
-
-function request(subUrl, callback, apiKey = "") {
-    if (ess) {
-        if (subUrl == "ipgeo" && sessionStorage.getItem("_ipGeolocation") && callback) {
-            callback(JSON.parse(sessionStorage.getItem("_ipGeolocation")));
-        } else if (subUrl == "timezone" && sessionStorage.getItem("_ipTimeZone") && callback) {
-            callback(JSON.parse(sessionStorage.getItem("_ipTimeZone")));
+    function request(subUrl, callback, apiKey = "") {
+        if (useSessionStorage) {
+            if (subUrl == "ipgeo" && sessionStorage.getItem("_ipGeolocation") && callback) {
+                callback(JSON.parse(sessionStorage.getItem("_ipGeolocation")));
+            } else if (subUrl == "timezone" && sessionStorage.getItem("_ipTimeZone") && callback) {
+                callback(JSON.parse(sessionStorage.getItem("_ipTimeZone")));
+            }
         }
-    }
-
-    var urlParameters = "";
-
-    if (!subUrl) {
-        callback(JSON.parse("{'status': 401, message: 'Given path to IP Geolocation API is not valid'}"));
-        return;
-    }
     
-    if (apiKey) {
-        urlParameters = addUrlParameter(urlParameters, "apiKey", apiKey);
-    }
-
-    if (excludesParameter) {
-        urlParameters = addUrlParameter(urlParameters, "excludes", excludesParameter);
-    }
+        var urlParameters = "";
+    
+        if (!subUrl) {
+            callback(JSON.parse("{'status': 401, message: 'Given path to IP Geolocation API is not valid'}"));
+            return;
+        }
         
-    if (fieldsParameter) {
-        urlParameters = addUrlParameter(urlParameters, "fields", fieldsParameter);
-    }
-
-    if (ipAddressParameter) {
-        urlParameters = addUrlParameter(urlParameters, "ip", ipAddressParameter);
-    }
-
-    if (langParameter) {
-        urlParameters = addUrlParameter(urlParameters, "lang", langParameter);
-    }
+        if (apiKey) {
+            urlParameters = addUrlParameter(urlParameters, "apiKey", apiKey);
+        }
     
-    if (tzParameter) {
-        urlParameters = addUrlParameter(urlParameters, "tz", tzParameter);
-    }
+        if (ipAddress) {
+            urlParameters = addUrlParameter(urlParameters, "ip", ipAddress);
+        }
+            
+        if (fields) {
+            urlParameters = addUrlParameter(urlParameters, "fields", fields);
+        }
     
-    if (latitudeParameter && longitudeParameter) {
-        urlParameters = addUrlParameter(urlParameters, "lat", latitudeParameter);
-        urlParameters = addUrlParameter(urlParameters, "long", longitudeParameter);
-    }
-
-    $.ajax ({
-        async: asyncParameter,
-        method: "GET",
-        url: "https://api.ipgeolocation.io/".concat(subUrl, urlParameters, ""),
-        contentType: "application/json",
-        dataType: "json",
-        success: function (result, status, xhr) {
-            if (ess) {
-                if (subUrl == "ipgeo") {
-                    sessionStorage.setItem("_ipGeolocation", JSON.stringify(result));
-                } else if (subUrl == "timezone") {
-                    sessionStorage.setItem("_ipTimeZone", JSON.stringify(result));
+        if (excludes) {
+            urlParameters = addUrlParameter(urlParameters, "excludes", excludes);
+        }
+    
+        if (lang) {
+            urlParameters = addUrlParameter(urlParameters, "lang", lang);
+        }
+        
+        if (tz) {
+            urlParameters = addUrlParameter(urlParameters, "tz", tz);
+        }
+        
+        if (latitude && longitude) {
+            urlParameters = addUrlParameter(urlParameters, "lat", latitude);
+            urlParameters = addUrlParameter(urlParameters, "long", longitude);
+        }
+    
+        $.ajax ({
+            async: asyncCall,
+            method: "GET",
+            url: "https://api.ipgeolocation.io/".concat(subUrl, urlParameters, ""),
+            contentType: "application/json",
+            dataType: "json",
+            success: function (result, status, xhr) {
+                if (useSessionStorage) {
+                    if (subUrl == "ipgeo") {
+                        sessionStorage.setItem("_ipGeolocation", JSON.stringify(result));
+                    } else if (subUrl == "timezone") {
+                        sessionStorage.setItem("_ipTimeZone", JSON.stringify(result));
+                    }
+                }
+    
+                if (callback) {
+                    callback(result);
+                }
+            },
+            error: function (xhr, status, error) {
+                if (callback) {
+                    callback(JSON.parse(xhr.responseText));
                 }
             }
+        });
+    }
 
-            if (callback) {
-                callback(result);
-            }
-        },
-        error: function (xhr, status, error) {
-            if (callback) {
-                callback(JSON.parse(xhr.responseText));
-            }
+    function addUrlParameter(parameters, parameterName, parameterValue) {
+        if (parameters) {
+            parameters = parameters.concat("&", parameterName, "=", parameterValue);
+        } else {
+            parameters = "?".concat(parameterName, "=", parameterValue);
         }
-    });
-}
+    
+        return parameters;
+    }
+
+    return {
+        enableSessionStorage: function(e = false) {
+            useSessionStorage = e;
+        },
+        makeAsyncCallsToAPI: function(a = true) {
+            asyncCall = a;
+        },
+        setIPAddress: function(ip = "") {
+            ipAddress = ip;
+        },
+        setFields: function(f = "") {
+            fields = f;
+        },
+        setExcludes: function(e = "") {
+            excludes = e;
+        },
+        setLanguage: function(l = "en") {
+            lang = l;
+        },
+        setTimeZone: function(t = "") {
+            tz = t;
+        },
+        setCoordinates: function(latitude = "", longitude = "") {
+            latitudeParameter = latitude;
+            longitudeParameter = longitude;
+        },
+        getGeolocation: function(callback, apiKey = "") {
+            request("ipgeo", callback, apiKey);
+        },
+        getTimezone: function(callback, apikey = "") {
+            request("timezone", callback, apikey);
+        }
+    }
+}();
